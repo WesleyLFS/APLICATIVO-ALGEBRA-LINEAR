8068804bc20d6f035ccaeaf21695cb255324a9b4
WesleyLFS <54479437+WesleyLFS@users.noreply.github.com>
2020-02-05 22:42:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,42 @@ class Gerenciador(ScreenManager):
 class Inicio(Screen):
     pass
 
+class Matriz(Screen):
+    pass
+
+class 1Matriz(Screen):
+    pass
+
+class 1M.Selecao(Screen):
+    pass
+
+class 1M.Valores(Screen):
+    pass
+
+class 1M.Resultado(Screen):
+    pass
+
+class 2Matriz(Screen):
+    pass
+
+class 2M.Selecao(Screen):
+    pass
+
+class 2M.Valores(Screen):
+    pass
+
+class 2M.Resultado(Screen):
+    pass
+
+class SL.Selecao(Screen):
+    pass
+
+class SL.Valores(Screen):
+    pass
+
+class SL.Resultado(Screen):
+    pass
+
 class Main(App):
 
     def build(self):
